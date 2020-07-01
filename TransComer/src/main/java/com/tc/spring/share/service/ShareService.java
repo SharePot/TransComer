849aad03474278538f3ec1672b1912b9db6f34c1
HyunnieKu <sese9ku@gmail.com>
2020-07-01 13:57:40 +0900

    Share, Study 인터페이스, 컨트롤러 작성

--- TransComer/src/main/java/com/tc/spring/share/service/ShareService.java
+++ TransComer/src/main/java/com/tc/spring/share/service/ShareService.java
@@ -1,5 +1,36 @@
 package com.tc.spring.share.service;
 
+import java.util.ArrayList;
+
+import com.tc.spring.share.domain.Share;
+
 public interface ShareService {
+	
+	/**
+	 * 자료공유 전체 리스트 조회
+	 * @return
+	 */
+	public ArrayList<Share> selectShareList();
+
+	/**
+	 * 자료공유 한개 상세 조회
+	 * @param shareNo
+	 * @return
+	 */
+	public Share selectShareOne(int shareNo);
+	
+	/**
+	 * 자료공유 삽입
+	 * @param share
+	 * @return
+	 */
+	public int insertShare(Share share);
+	
+	/**
+	 * 자료공유 삭제
+	 * @param shareNo
+	 * @return
+	 */
+	public int deleteShare(int shareNo);
 
 }
--- TransComer/src/main/java/com/tc/spring/study/controller/StudyController.java
+++ TransComer/src/main/java/com/tc/spring/study/controller/StudyController.java
@@ -1,8 +1,14 @@
 package com.tc.spring.study.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.tc.spring.study.domain.Study;
 import com.tc.spring.study.service.StudyService;
 
 @Controller
@@ -10,4 +16,28 @@ public class StudyController {
 
 	@Autowired
 	private StudyService studyService;
+
+	public ModelAndView studyList(ModelAndView mv) {
+		return mv;
+	}
+
+	public String studySearch(Study study, Model model) {
+		return "";
+	}
+
+	public String studyDetail(int studyNo, Model model) {
+		return "";
+	}
+
+	public String insertStudy(Study study, Model model) {
+		return "";
+	}
+
+	public String updateStudy(Study study, Model model, HttpServletRequest request) {
+		return "";
+	}
+
+	public String deleteStudy(int shareNo, Model model, HttpServletRequest request, RedirectAttributes rd) {
+		return "";
+	}
 }
