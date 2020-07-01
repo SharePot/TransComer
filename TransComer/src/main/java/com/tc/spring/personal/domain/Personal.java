d38d965c7eccddc48b6e9e15a2c82b4474bea827
Angela <user1@user1-PC>
2020-07-01 14:02:11 +0900

    PSERSONAL 정리

--- TransComer/src/main/java/com/tc/spring/personal/domain/Personal.java
+++ TransComer/src/main/java/com/tc/spring/personal/domain/Personal.java
@@ -1,5 +1,133 @@
 package com.tc.spring.personal.domain;
 
+import java.sql.Date;
+
 public class Personal {
+
+	private int personalNo;
+	private String personTitle;
+	private Date personalDate;
+	private String personalWriter;
+	private String personalPLang;
+	private String personalTLang;
+	private int personalSchedule;
+	private int personalPrice;
+	private String personalCount;
+	private String personalDStatus;
+	private String powerYN;
+
+	public Personal(int personalNo, String personTitle, Date personalDate, String personalWriter, String personalPLang,
+			String personalTLang, int personalSchedule, int personalPrice, String personalCount, String personalDStatus,
+			String powerYN) {
+		super();
+		this.personalNo = personalNo;
+		this.personTitle = personTitle;
+		this.personalDate = personalDate;
+		this.personalWriter = personalWriter;
+		this.personalPLang = personalPLang;
+		this.personalTLang = personalTLang;
+		this.personalSchedule = personalSchedule;
+		this.personalPrice = personalPrice;
+		this.personalCount = personalCount;
+		this.personalDStatus = personalDStatus;
+		this.powerYN = powerYN;
+	}
+
+	public int getPersonalNo() {
+		return personalNo;
+	}
+
+	public void setPersonalNo(int personalNo) {
+		this.personalNo = personalNo;
+	}
+
+	public String getPersonTitle() {
+		return personTitle;
+	}
+
+	public void setPersonTitle(String personTitle) {
+		this.personTitle = personTitle;
+	}
+
+	public Date getPersonalDate() {
+		return personalDate;
+	}
+
+	public void setPersonalDate(Date personalDate) {
+		this.personalDate = personalDate;
+	}
+
+	public String getPersonalWriter() {
+		return personalWriter;
+	}
+ 
+	public void setPersonalWriter(String personalWriter) {
+		this.personalWriter = personalWriter;
+	}
+
+	public String getPersonalPLang() {
+		return personalPLang;
+	}
+
+	public void setPersonalPLang(String personalPLang) {
+		this.personalPLang = personalPLang;
+	}
+
+	public String getPersonalTLang() {
+		return personalTLang;
+	}
+
+	public void setPersonalTLang(String personalTLang) {
+		this.personalTLang = personalTLang;
+	}
+
+	public int getPersonalSchedule() {
+		return personalSchedule;
+	}
+
+	public void setPersonalSchedule(int personalSchedule) {
+		this.personalSchedule = personalSchedule;
+	}
+
+	public int getPersonalPrice() {
+		return personalPrice;
+	}
+
+	public void setPersonalPrice(int personalPrice) {
+		this.personalPrice = personalPrice;
+	}
+
+	public String getPersonalCount() {
+		return personalCount;
+	}
+
+	public void setPersonalCount(String personalCount) {
+		this.personalCount = personalCount;
+	}
+
+	public String getPersonalDStatus() {
+		return personalDStatus;
+	}
+
+	public void setPersonalDStatus(String personalDStatus) {
+		this.personalDStatus = personalDStatus;
+	}
+
+	public String getPowerYN() {
+		return powerYN;
+	}
+
+	public void setPowerYN(String powerYN) {
+		this.powerYN = powerYN;
+	}
+
+	@Override
+	public String toString() {
+		return "Personal [personalNo=" + personalNo + ", personTitle=" + personTitle + ", personalDate=" + personalDate
+				+ ", personalWriter=" + personalWriter + ", personalPLang=" + personalPLang + ", personalTLang="
+				+ personalTLang + ", personalSchedule=" + personalSchedule + ", personalPrice=" + personalPrice
+				+ ", personalCount=" + personalCount + ", personalDStatus=" + personalDStatus + ", powerYN=" + powerYN
+				+ "]";
+	}
 
 }
--- TransComer/src/main/java/com/tc/spring/personal/service/PersonalService.java
+++ TransComer/src/main/java/com/tc/spring/personal/service/PersonalService.java
@@ -1,5 +1,43 @@
 package com.tc.spring.personal.service;
 
+import java.util.ArrayList;
+
+import com.tc.spring.personal.domain.Personal;
+
 public interface PersonalService {
 
+   public ArrayList<Personal> selectList();
+   /**
+    * 1:1의뢰 게시판 전체 조회 서비스
+    * @param 
+    * @return list
+    */
+   
+   public Personal selectOne();
+   /**
+    * 1:1의뢰 게시판 상세조회용 서비스
+    * @param 
+    * @return
+    */
+   
+   public int insertPersonl();
+   /**
+    * 1:1의뢰 게시판 등록 서비스
+    * @param 
+    * @return
+    */
+   
+   public int updatePersonal();
+   /**
+    * 1:1의뢰 게시판 수정용 서비스
+    * @param 
+    * @return
+    */
+   
+   public int deletePersonal() ;
+   /**
+    * 1:1의뢰 게시판 삭제용 서비스
+    * @param 
+    * @return
+    */
 }
