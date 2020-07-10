86c891328f63692cd3661f9dadacccfda81066d6
HyunnieKu <sese9ku@gmail.com>
2020-07-10 15:28:10 +0900

    servlet-context, web.xml 웹소켓 세팅 설정

--- TransComer/src/main/java/com/tc/spring/chat/controller/ChatController.java
+++ TransComer/src/main/java/com/tc/spring/chat/controller/ChatController.java
@@ -2,6 +2,7 @@ package com.tc.spring.chat.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.tc.spring.chat.service.ChatService;
 
@@ -10,4 +11,10 @@ public class ChatController {
 
 	@Autowired
 	private ChatService chatService;
+	
+	// 채팅 페이지로 이동시킴
+	@RequestMapping("wsclient.kh")
+	public String chatForm() {
+		return "chat/wsclient";
+	}
 }
---
+++ TransComer/src/main/java/com/tc/spring/chat/interceptor/HandshakeInterceptor.java
@@ -0,0 +1,5 @@
+package com.tc.spring.chat.interceptor;
+
+public class HandshakeInterceptor {
+
+}
